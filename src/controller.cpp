dc9c3efe443e407b3f06ac151bc765d8b7ddd1d5
Hannah Sullivan <62521761+hanners2@users.noreply.github.com>
2025-01-10 10:08:17 -0600

    Add HandleDifficultySelection function to controller

--- src/controller.cpp
+++ src/controller.cpp
@@ -1,5 +1,6 @@
 #include "controller.h"
 
+#include "gamesetup.h"
 #include "snake.h"
 
 #include "SDL.h"
@@ -16,6 +17,32 @@ void Controller::ChangeDirection(Snake &snake, Snake::Direction input,
                                  Snake::Direction opposite) const {
   if (snake.direction != opposite || snake.size == 1) snake.direction = input;
   return;
+}
+
+void Controller::HandleDifficultySelection(bool &waiting, bool &running,
+                                           GameSetup *game_setup) {
+  SDL_Event e;
+  while (SDL_PollEvent(&e)) {
+    if (e.type == SDL_QUIT) {
+      running = false;
+    } else if (e.type == SDL_MOUSEBUTTONDOWN) {
+      // Figure out if this click was on a button and set the appropriate
+      // difficulty if so
+      if (e.button.button == SDL_BUTTON_LEFT) {
+        // See if the click was on any of the buttons
+        if (onButton(e.button.x, e.button.y, game_setup->easyButton)) {
+          game_setup->SetDifficulty(DifficultyLevels::kEasy);
+          waiting = false;
+        } else if (onButton(e.button.x, e.button.y, game_setup->medButton)) {
+          game_setup->SetDifficulty(DifficultyLevels::kMedium);
+          waiting = false;
+        } else if (onButton(e.button.x, e.button.y, game_setup->hardButton)) {
+          game_setup->SetDifficulty(DifficultyLevels::kHard);
+          waiting = false;
+        }
+      }
+    }
+  }
 }
 
 void Controller::HandleInput(bool &running, Snake &snake) const {
--- src/controller.h
+++ src/controller.h
@@ -1,11 +1,14 @@
 #ifndef CONTROLLER_H
 #define CONTROLLER_H
 
+#include "gamesetup.h"
 #include "snake.h"
 
 class Controller {
  public:
   void HandleInput(bool &running, Snake &snake) const;
+  void HandleDifficultySelection(bool &waiting, bool &running,
+                                 GameSetup *game_setup);
 
  private:
   void ChangeDirection(Snake &snake, Snake::Direction input,
