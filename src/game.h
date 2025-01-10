80e106e6da55c3fd32a9573c8f9c9b94a92c5af1
Hannah Sullivan <62521761+hanners2@users.noreply.github.com>
2025-01-10 16:19:56 -0600

    Minor formatting change in game.h

--- src/game.h
+++ src/game.h
@@ -10,7 +10,7 @@
 
 class Game {
 public:
-  Game(std::size_t grid_width, std::size_t grid_height, GameSetup& game_setup);
+  Game(std::size_t grid_width, std::size_t grid_height, GameSetup &game_setup);
   void Run(Controller const &controller, Renderer &renderer,
            std::size_t target_frame_duration);
   int GetScore() const;
